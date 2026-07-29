75e5f2b0149d6ea9252fca5501895dcd57ad5e94
ttynico <132922088+ttynico@users.noreply.github.com>
2026-07-29 21:15:06 +0000

    Add 06-Data-Blocks

---
+++ 06-Data-Blocks/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.19.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.0.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
